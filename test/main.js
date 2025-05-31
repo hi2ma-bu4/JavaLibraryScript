2bc9fce770e1e7085c8fcbd252c535f6581e6429
hi2ma-bu4 <hi2ma.bu4.bot@gmail.com>
2025-06-01 01:03:55 +0900

    🐛 デモを充実

--- test/main.js
+++ test/main.js
@@ -1,12 +1,8 @@
 try {
 	// IDEの型表示用
 	const { default: JavaLibraryScript } = require("../types/JavaLibraryScript");
+	console.log("is load?!");
 } catch (e) {}
-const worker = new Worker("./worker.js");
-
-setInterval(() => {
-	//worker.terminate();
-}, 1000);
 
 jasc.on("DOMContentLoaded", () => {
 	const log = new JavaLibraryScript.libs.sys.Logger("", JavaLibraryScript.libs.sys.Logger.LOG_LEVEL.TIME);
@@ -19,12 +15,5 @@ jasc.on("DOMContentLoaded", () => {
 	JavaLibraryScript.math.BigFloat.config.allowPrecisionMismatch = true;
 	//JavaLibraryScript.math.BigFloat.config.roundingMode = JavaLibraryScript.math.BigFloatConfig.ROUND_HALF_UP;
 
-	const { BigFloat, bigFloat } = JavaLibraryScript.math;
-	window.BigFloat = BigFloat;
-	window.bigFloat = bigFloat;
-
-	log.hr();
-	log.log(bigFloat(3, 30).sqrt().toString());
-	log.log(BigFloat.pi(30).toString());
-	log.hr();
+	const c = new Calculator("#root");
 });
